bc9c907cb8a44ac8145f0a39cbe783e1352caa1e
blank <sch1111878@126.com>
2015-11-02 18:35:25 +0800

    [what] 新建工程 导入AFNetworking

---
+++ MyTools/MyTools/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTools
+//
+//  Created by blank on 15/11/2.
+//  Copyright © 2015年 blank. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
